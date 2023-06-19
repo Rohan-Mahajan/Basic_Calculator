e64c3573960c51f08b91358e1d0de5132c8b4b4c
Rohan-Mahajan <rohumahajan0707@gmail.com>
2023-06-20 00:25:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const screenDisplay = document.querySelector('.screen')
+const buttons = document.querySelectorAll('button')
+
+let calculation = []
+let accumulativeCalculation;
+
+function calculate(button){
+    const value = button.textContent;
+
+    if(value === "Clear"){
+        calculation = [];
+        screenDisplay.textContent = "0";
+    }
+
+    else if(value === "="){
+        screenDisplay.textContent = eval(accumulativeCalculation);
+        calculation = [];
+        accumulativeCalculation = "";
+    }
+
+    else{
+        calculation.push(value);
+        accumulativeCalculation = calculation.join('');
+        screenDisplay.textContent = accumulativeCalculation;
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click', ()=> calculate(button)));
